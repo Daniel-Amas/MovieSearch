be8e93197a5a43390829f92e3f92727dd1d8386a
Daniel Amas <amasdaniel3@gmail.com>
2023-11-06 21:09:57 -0500

    fixed search icon bug

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import MovieCard from "./MovieCard";
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=d4ea9037';
 
-const movie1 = {
-    "Title": "Superman, Spiderman or Batman",
-    "Year": "2011",
-    "imdbID": "tt2084949",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-}
-
 
 const App = () => {
 
@@ -42,7 +34,7 @@ const App = () => {
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
-                    src="{SearchIcon}"
+                    src={SearchIcon}
                     alt="search"
                     onClick={() => searchMovies(searchTerm)}
                 />
